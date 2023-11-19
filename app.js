a92e3ec313ae31173b030c2a2034c3e2bf87f4e8
Zifei Song <song.zif@northeastern.edu>
2023-11-18 22:35:21 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import Hello from "./hello.js";
-import Lab5 from "./lab5.js";
+import Lab5 from "./Lab5.js";
 import cors from 'cors';
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
