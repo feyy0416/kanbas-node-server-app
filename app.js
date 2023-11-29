a62aa2287980a35fc56f194e899b5898b3443ff3
Zifei Song <song.zif@northeastern.edu>
2023-11-28 21:55:45 -0500

    add connection to db

--- app.js
+++ app.js
@@ -6,10 +6,33 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from './assignments/routes.js';
 import "dotenv/config";
-
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+import "dotenv/config";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: process.env.FRONTEND_URL
+}));
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+
 app.use(express.json());
+UserRoutes(app);
 Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
