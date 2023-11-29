e484d143ada21b3b1d8e50564bf123f5ab2d68a2
Zifei Song <song.zif@northeastern.edu>
2023-11-28 23:02:37 -0500

    added connection string

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 import session from "express-session";
 import "dotenv/config";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
   credentials: true,
